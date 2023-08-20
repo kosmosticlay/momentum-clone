7eb9aab638b56f2ca0ad424af5da7ec0b3b19ac6
cosmosunion <kosmosticlay@gmail.com>
2023-08-20 19:34:05 +0900

    final update
    Youtube Player error(6s) solved

--- js/asmr.js
+++ js/asmr.js
@@ -30,10 +30,10 @@ function onPlayerReady(event) {
 //    the player should play for six seconds and then stop.
 var done = false;
 function onPlayerStateChange(event) {
-  if (event.data == YT.PlayerState.PLAYING && !done) {
-    setTimeout(stopVideo, 6000);
-    done = true;
-  }
+  // if (event.data == YT.PlayerState.PLAYING && !done) {
+  //   setTimeout(stopVideo, 6000);
+  //   done = true;
+  // }
 }
 function stopVideo() {
   player.stopVideo();
